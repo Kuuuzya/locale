625711e22e59715fb5088465b80ac7a97d3a9ba9
Kuuuzya <kuuuzya@ya.ru>
2024-11-10 07:06:36 +0300

    Initial commit 2

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://app:cWn4zqus1!@localhost/locale"
+load_dotenv()  # Загружает переменные из .env в окружение
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
